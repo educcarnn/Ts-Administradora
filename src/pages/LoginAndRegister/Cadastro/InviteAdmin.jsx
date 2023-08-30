ecb62eb76eb592e61090f949486c81c8251e66c7
educc <educcarnn@gmail.com>
2023-08-30 09:46:42 -0300

    fix: url

--- src/pages/LoginAndRegister/Cadastro/InviteAdmin.jsx
+++ src/pages/LoginAndRegister/Cadastro/InviteAdmin.jsx
@@ -1,74 +1,130 @@
-import React, { useState, useEffect } from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 import { API_URL } from "../../../db/Api";
 import { DashboarDiv } from "../../Dashboard/style";
-import axios from "axios";
+import videoBackground from "../../../assets/Videos/telaLogin.mp4"
+import { toast } from 'react-toastify';
+import { isExpired } from 'react-jwt';
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const VideoBG = styled.video`
+  position: fixed;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100vh;
+  z-index: -1;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Input = styled.input`
+  padding: 10px 15px;
+  margin: 10px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+  max-width: 300px;
+  box-sizing: border-box;
+`;
+
+const Text = styled.p`
+  
+  color: white;
+  font-weight: bold;
+`
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
 
 export default function InviteAdmin() {
+
   const history = useHistory();
   const location = useLocation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState("");
-  const [tokenValid, setTokenValid] = useState(false);
+
 
   useEffect(() => {
     const token = new URLSearchParams(location.search).get("token");
 
     if (token) {
-
-      localStorage.setItem("token", token);
-      setTokenValid(true);
+      if(isExpired(token)) {
+        toast.error("O token expirou.");
+        history.push("/");
+      }
     } else {
-
-      history.push("/"); 
+      toast.error("Token não fornecido.");
+      history.push("/");
     }
   }, [history, location.search]);
 
+
   const handleRegister = async () => {
     try {
-      const response = await axios.post(`${API_URL}/admin/register`, {
+      const response = await API_URL.post("/admin/register", {
         email,
         password,
-        role: 'admin'
+        role: "admin",
       });
-
-      if (response.status === 200) {
-
-        history.push("/"); // Substitua pela sua rota
-      } else {
   
-        console.error("Erro no registro");
+      if (response.status === 201) {
+        toast.success("Registro realizado com sucesso!"); // Notificação de sucesso
+        setTimeout(() => {
+          history.push("/"); // Substitua pela sua rota
+        }, 2500); // Redireciona após 2,5 segundos
+      } else {
+        console.error("Erro no registro. Resposta:", response);
+        toast.error("Erro ao realizar o registro."); // Notificação genérica de erro
       }
     } catch (error) {
-      console.error("Erro no registro:", error);
+      console.error("Erro no registro:", error.response ? error.response.data : error.message);
+      const errorMessage = error.response?.data?.message || "Erro desconhecido.";
+      toast.error("Erro ao realizar o registro: " + errorMessage); // Notificação de erro com a mensagem específica
     }
   };
-
+  console.log(email)
+  console.log(password)
   return (
     <div>
-      <DashboarDiv>
-        <div>Ts Administradora - Cadastro de Admins</div>
-      </DashboarDiv>
-      {tokenValid && (
-        <div>
-          <p>Complete o registro:</p>
-          <input
-            type="email"
-            placeholder="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-    
-          <button onClick={handleRegister}>Registrar</button>
-        </div>
-      )}
+      <DashboarDiv>Ts Administradora - Cadastro de Admins</DashboarDiv>
+      <VideoBG autoPlay loop muted>
+        <source src={videoBackground} type="video/mp4" />
+        Seu navegador não suporta a tag de vídeo.
+      </VideoBG>
+      <Container>
+        <Text>Complete o registro</Text>
+        <Input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={handleRegister}>Registrar</Button>
+      </Container>
     </div>
   );
 }
