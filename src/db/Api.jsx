1d4d252a2a55af0d7ae9ed4bcaa685cc1ded61bb
educc <educcarnn@gmail.com>
2023-08-30 15:38:08 -0300

    feat: link

--- src/db/Api.jsx
+++ src/db/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://tsadministradora.onrender.com",
   timeout: 10000, 
   headers: {
     "Content-Type": "application/json",
