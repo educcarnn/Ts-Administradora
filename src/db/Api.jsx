1ffc0ebd91c41f6c1a2bdc1e939fc954d312ce02
educcarnn <educcarnn@gmail.com>
2023-09-12 16:53:01 -0300

    fix: api

--- src/db/Api.jsx
+++ src/db/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://tsadministradoraback.onrender.com",
   timeout: 10000, 
   
   headers: {
